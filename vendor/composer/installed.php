cfefb13f9c781fd49e09c44d3ae6615b41d54e2a
Jérôme Odier <jerome.odier@lpsc.in2p3.fr>
2022-07-29 00:02:20 +0200

    update

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'grav-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '0ea7cec0923f7f7fed94cc6d8408bacf13dcdf73',
+        'name' => 'jodier/themes-from-git',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'jodier/themes-from-git' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'grav-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '0ea7cec0923f7f7fed94cc6d8408bacf13dcdf73',
+            'dev_requirement' => false,
+        ),
+    ),
+);
